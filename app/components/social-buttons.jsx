b6b2c4e720d4bb284f72bb84889bc174b013ff11
Josh Stovall <joshstovall@gmail.com>
2025-01-12 00:00:19 -0700

    fix typo

--- app/components/social-buttons.jsx
+++ app/components/social-buttons.jsx
@@ -6,7 +6,7 @@ export default function SocialButtons() {
 
     return (
         <div className="flex flex-col justify-center items-center space-y-4">
-            <Link href="https://github.com/joshstovall/gode.site">
+            <Link href="https://github.com/joshstovall/gcode.site">
                 <button className="bg-gray-300 hover:bg-blue-700 text-black font-bold py-2 px-4 rounded-full">
                     Fork on Github
                 </button>
