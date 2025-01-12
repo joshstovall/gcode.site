40e75443b86d7c5fbb14f8db99318280d2786f96
Josh Stovall <joshstovall@gmail.com>
2025-01-11 23:52:02 -0700

    added missing file

---
+++ app/components/social-buttons.jsx
@@ -0,0 +1,17 @@
+'use client'
+
+import Link from "next/link"
+
+export default function SocialButtons() {
+
+    return (
+        <div className="flex flex-col justify-center items-center space-y-4">
+            <Link href="https://github.com/joshstovall/gode.site">
+                <button className="bg-gray-300 hover:bg-blue-700 text-black font-bold py-2 px-4 rounded-full">
+                    Fork on Github
+                </button>
+            </Link>
+        </div>
+    )
+
+}
